38f8031ea2ba11c963e24bedf3d060ffe88d2d3c
unknown <C:\Users\Tihanovich\AppData\Roaming\The Bat!>
2013-11-12 10:37:30 +0400

    new up

--- filter.catalog.js
+++ filter.catalog.js
@@ -1,7 +1,29 @@
 var filter_array = {};
 $(function(){
 
-   
+
+
+    function CheckBox(keys,values){
+        values = values.split(',');
+        // console.log(values);
+//var url = window.location.hash.substring(1);
+        $('.tb__filter-type').each(function(){
+            if($(this).attr('data-ft') == keys){
+                $(this).find('input').each(function(){
+                    var checks = $.inArray($(this).attr('data-prop'), values);
+                    if(checks >= 0){
+                        $(this).attr('checked', true);
+
+                    }
+
+
+                })
+            }
+
+        })
+        //console.log(keys);
+        //console.log(values);
+    }
 })
     function CheckBox(keys,values){
         values = values.split(',');
